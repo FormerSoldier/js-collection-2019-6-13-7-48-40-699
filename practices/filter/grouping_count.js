0c4869f06f1e66e75eda729e21b0a909b5b3592a
GARSON ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGGA5@OOCL.COM>
2019-07-10 18:01:18 +0800

    feat: implements most of function

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,19 @@
 function grouping_count(collection) {
 
   //implement here
+  var result = {};
+  collection.forEach((item) => {
+       let flag = false;
+       for(let key in result){
+          if(parseInt(key) == item){
+             flag = true;
+             result[`"${item}"`] ++;
+          }
+          if(!flag)
+            result[`"${item}"`] = 1;
+       } 
+  });
+  return result;
 }
 
 module.exports = grouping_count;
